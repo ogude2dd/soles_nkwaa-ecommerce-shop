b4061307491d9358d6e6947806a371ad8990dbfd
ogude2dd <ogude2dd@gmail.com>
2026-04-19 13:05:44 +0000

    Intro page and home page with About and Logout feature

---
+++ android/app/src/main/kotlin/com/example/solesnkwaa/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.solesnkwaa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 25.0,
+          ),
+          child: Column(
+            mainAxisAlignment:
+                MainAxisAlignment.center,
+            children: [
+              // logo
+              Padding(
+                padding: const EdgeInsets.all(
+                  25.0,
+                ),
+                child: Image.asset(
+                  'lib/images/soles_nkwaa.png',
+                  height: 240,
+                ),
+              ),
+
+              const SizedBox(height: 48),
+
+              // title
+              const Text(
+                'Own Every Step',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Color(0xFF545454),
+                ),
+              ),
+
+              const SizedBox(height: 24),
+
+              //sub title
+              const Text(
+                'Brand new sneakers and custom kicks made with premium quality',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey,
+                ),
+                textAlign: TextAlign.center,
+              ),
+
+              const SizedBox(height: 48),
+
+              //start now buttom
+              GestureDetector(
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                       const HomePage(),
+                  ),
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[900],
+                    borderRadius:
+                        BorderRadius.circular(12),
+                  ),
+                  padding: const EdgeInsets.all(
+                    25,
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'Shop Now',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight:
+                            FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
